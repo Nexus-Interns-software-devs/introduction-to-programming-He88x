dfa956b0da0960beb9dd0dad28bc15e6fafa16f5
He88x <hameedz069@gmail.com>
2026-07-25 23:42:06 +0100

    LAB-02: Operators and Expressions

---
+++ CONTROL STRUCTURE/LAB-02/code/operators.js
@@ -0,0 +1,32 @@
+//Arithmetic Operations:
+// Calculating the sum of two numbers
+let sum1 = 20;
+let sum2 = 11;
+let sum = sum1 + sum2;
+console.log(`Sum: ${sum}`);
+
+//calculating the product of two numbers
+let product = sum1 * sum2;
+console.log(`Product: ${product}`);
+
+//Calculate the remainder of division (modulo)
+let remainder = sum1 % sum2;
+console.log(`Remainder: ${remainder}`);
+
+
+
+//Using Comparison Operations:
+let a = 20;
+let b = 10;
+console.log(`Is Equal: ${a === b}`);
+console.log(`Is Greater: ${a > b}`);
+console.log(`Both True: ${a > b && b < a}`);
+
+//Using Logical Operations
+let num1 = 10;
+let num2 = 20;
+console.log(`FOR AND: ${num1 > 5 && num2 < 30}`);
+console.log(`FOR OR: ${num1 > 5 || num2 < 10}`);
+//Log for the boolean results
+console.log(`Both False: ${num1 < 5 && num2 > 30}`);
+console.log(`Either True: ${num1 > 5 || num2 < 30}`);
